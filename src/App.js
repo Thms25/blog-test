53887a258b4edcf7fff92e67ff37ad43bba1b9b7
thomas allen <thomasallenmartinho@gmail.com>
2023-06-27 15:04:58 +0200

    halfway through tuto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+  const myTitle = "Welcome to my blog";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
+          <h1>
+            {myTitle}
+          </h1>
+        </header>
+        <main>
+          <Home />
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs")
+
+  return (
+    <div className="home">
+      { error && <div><h1>{ error }</h1></div> }
+      { isPending && <div><h1>loading blogs...</h1></div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
+    </div>
+   );
+}
+
+export default Home;
